1c61756c042df9ed3c443feb384ad46ade9a9f51
Jason Thatcher <jasont14@gmail.com>
2015-05-05 21:39:52 -0500

    Add Bubble and Linear Sort

---
+++ Sort/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] myNumbers = { 1, 3, 2, 5, 8, 9, 4, 6, 10, 7, 11, 12, 20, 13, 15, 21, 22, 14, 16, 23, 24, 17, 25, 18, 19 };
+            CustomSort mySort = new CustomSort();
+
+            Console.WriteLine("LINEAR SORT");
+            mySort.LinearSort(myNumbers);
+
+            int[] myNumbers2 = { 1, 3, 2, 5, 8, 9, 4, 6, 10, 7, 11, 12, 20, 13, 15, 21, 22, 14, 16, 23, 24, 17, 25, 18, 19 };
+
+            Console.WriteLine("\nBUBBLE SORT");
+            mySort.BubbleSort(myNumbers2);
+
+            int[] myNumbers3 = { 1, 3, 2, 5, 8, 9, 4, 6, 10, 7, 11, 12, 20, 13, 15, 21, 22, 14, 16, 23, 24, 17, 25, 18, 19 };
+
+            Console.WriteLine("\nBUBBLE SORT WITH BREAK");
+            mySort.BubbleSortWithBreak(myNumbers3);
+
+            Console.ReadLine();
+        }
+    }
+}
+
---
+++ Sort/Sort.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sort
+{
+    class CustomSort
+    {
+        public CustomSort()
+        {
+
+        }
+
+        public void LinearSort(int[] intArray)
+        {
+            int minValue = 0;
+            int temp = 0;
+
+            int[] TempArray = intArray;
+
+            for (int i = 0; i < intArray.Length; i++)
+            {
+                minValue = intArray[i];
+
+                int jLocation = 0;
+
+                for (int j = i + 1; j < intArray.Length; j++)
+                {
+                    if (TempArray[j] < minValue)
+                    {
+                        minValue = TempArray[j];
+                        jLocation = j;
+                    }
+                }
+
+                if (jLocation > 0)
+                {
+                    temp = intArray[i];
+                    TempArray[i] = minValue;
+                    TempArray[jLocation] = temp;
+                }
+
+                WriteArray(TempArray);
+
+            }
+        }
+        public void BubbleSort(int[] intArray2)
+        {
+            int[] TempArray2 = intArray2;
+
+            for (int i = 0; i < intArray2.Length; i++ )
+            {
+                WriteArray(TempArray2);
+
+                for (int j = i; j < intArray2.Length-1; j++)
+                {
+                    if (TempArray2[j] > TempArray2[j+1])
+                    {
+                        int temp21 = TempArray2[j];
+                        TempArray2[j] = TempArray2[j+1];
+                        TempArray2[j+1] = temp21;
+                    }
+                }
+            }              	       
+        }
+
+        public void BubbleSortWithBreak(int[] intArray3)
+        {
+            int[] TempArray3 = intArray3;
+
+            bool SortHappen;
+
+            do
+            {
+                WriteArray(TempArray3);
+                SortHappen = false;
+
+                for (int j = 0; j < intArray3.Length - 1; j++)
+                {
+                    if (TempArray3[j] > TempArray3[j + 1])
+                    {
+                        int temp = TempArray3[j];
+                        TempArray3[j] = TempArray3[j + 1];
+                        TempArray3[j + 1] = temp;
+                        SortHappen = true;
+                    }
+
+                } 
+            } while (SortHappen);
+              
+
+        }
+
+        public void WriteArray(int[] ArrayToWrite)
+        {
+            
+            for (int i = 0; i < ArrayToWrite.Length; i++)
+            {
+                if (i == ArrayToWrite.Length - 1)
+                {
+                    Console.Write(ArrayToWrite[i]+"\n");
+                }
+                else
+                {
+                    Console.Write(ArrayToWrite[i] + ",");
+                }
+
+            }
+
+        }
+
+    }
+}
